981c69933bc77d07c29bbe620804ab0960d69166
hiep0218 <hiep0218@gmail.com>
2025-01-02 17:41:02 +0700

    update test 4_2

--- test_4_2/src/main.rs
+++ test_4_2/src/main.rs
@@ -13,15 +13,46 @@ fn main() {
     let mut s2 = String::from("hello");
     mutable_change(&mut s2);
     println!("s2= {s2}");
+
+    let mut s3 = String::from("hello");
+    let r2 = &mut s3;
+    let r3 = &mut s3; // chua error vi tham chieu chua duoc su dung
+    // println!("r2 = {r2}"); 
+    // println!("r3 = {r3}");
+    // Error co 2 tham chieu co the thay doi gia tri(&mut)r2,r3 den s3 ton tai 
+    // dong thoi dan den data race
+
+    let mut s4 = String::from("hello");
+    let r4 = &mut s4;
+    println!("r4 = {r4}");
+    let r5 = &mut s4;
+    println!("r5 = {r5}");
+    // Khong bi error vi pham vi su dung cua r4 da ket thuc.
+
+
+    let mut s5 = String::from("hello");
+    {
+        let r6 = &mut s5;
+    } // r6 goes out of scope here, so we can make a new reference with no problems.
+    let r7 = &mut s5;
+    println!("r7 = {r7}");
+
+
+    let mut s6 = String::from("hello");
+    let r8 = &s6; //no problem
+    let r9 = &s6; // no problem
+    let r10 = &mut s6; // BIG PROBLEM vi khong the tham chieu mut trong khi
+    // dang tham chieu notmut toi 1 gia tri
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
 
-fn not_change(some_string: &String) {
-    some_string.push_str(",world");
-}
+// fn not_change(some_string: &String) {
+//     some_string.push_str(",world");
+
+// }
 
 fn mutable_change(some_string: &mut String) {
     some_string.push_str(",world");
