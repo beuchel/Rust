9d2dd67df820f1c68fb052485b1416fdb5a7790f
hiep0218 <hiep0218@gmail.com>
2025-01-07 17:45:41 +0700

    add test 4_3

--- test_4_3/src/main.rs
+++ test_4_3/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+// The slice type
+    let mut s = String::from("hello world");
+
+    let word = firstword(&s);
+    s.clear();
+    println!("word = {word}");
+    
+}
+
+fn firstword (s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
---
+++ test_4_3/target/debug/test_4_3.d
@@ -0,0 +1,1 @@
+D:\Rust\git_rust\Rust\test_4_3\target\debug\test_4_3.exe: D:\Rust\git_rust\Rust\test_4_3\src\main.rs
