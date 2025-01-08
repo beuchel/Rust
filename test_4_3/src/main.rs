88043b5bb6782df23d01e58a7c7ac3782a10e26f
hiep0218 <hiep0218@gmail.com>
2025-01-08 17:42:53 +0700

    update test 4_3

--- test_4_3/src/main.rs
+++ test_4_3/src/main.rs
@@ -1,10 +1,17 @@
 fn main() {
-// The slice type
     let mut s = String::from("hello world");
-
     let word = firstword(&s);
     s.clear();
     println!("word = {word}");
+
+// The slice type la tham chieu den 1 phan cua string.
+    let str = String::from("hello world");
+    let slice = &str[0..2];
+    let words = &str[..5];
+    let words2 = &str[5]
+    println!("slice = {slice}");
+    println!("word = {words}");
+
     
 }
 
