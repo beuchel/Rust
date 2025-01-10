c7cf6fb4eda3feb21fff8345f1d456c9d7af1c6e
hiep0218 <hiep0218@gmail.com>
2025-01-10 17:33:21 +0700

    add test 5_1

--- test_5_1/src/main.rs
+++ test_5_1/src/main.rs
@@ -1,10 +1,52 @@
 fn main() {
     println!("Hello, world!");
+    let mut user1 = User {
+        active: true,
+        username: String::from("hiep"),
+        email: String::from("hiep0218@gmail.com"),
+        sign_in_count: 1,
+    };
+    user1.email = String::from("hiepnd0218@gmail.com");
+    println!("mail of username1 is {}",user1.email);
+
+//struct update syntax.
+//not update syntax.
+    let mut user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("hiep222@gmail.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+// after use update syntax.
+    let mut user3 = user {
+        email: String::from("abc@gmail.com"),
+        ..user1
+    };
 }
 
 struct User {
-    active: bool;
-    username: String;
-    email: String;
-    sign_in_count: u64;
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
+
+fn build_user(email: String, username: String) -> User{
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+// using the field init shorland. Chi can dat bien tham so trung voi bien cua struct.
+fn build_user1(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
---
+++ test_5_1/target/debug/test_5_1.d
@@ -0,0 +1,1 @@
+D:\Rust\git_rust\Rust\test_5_1\target\debug\test_5_1.exe: D:\Rust\git_rust\Rust\test_5_1\src\main.rs
