00145f22f37f882f55828d167471498b672a206e
hiep0218 <hiep0218@gmail.com>
2025-01-22 17:36:56 +0700

    update test 5_2

--- test_5_2/src/main.rs
+++ test_5_2/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+// Bai toan tinh dien tich hcn.
+
+// use Tuples.
+let rect1 = (30,50);
+println!("The area of the rectangle is {} square pixels.",area(rect1));
+}
+
+fn area(dimension: (u32, u32)) -> u32 {
+    dimension.0 * dimension.1
 }
