cf2cc2c54e52299450bd4a253e95c6cb46a64065
hiep0218 <hiep0218@gmail.com>
2025-02-10 17:33:51 +0700

    update test 5_3

--- test_5_3/src/main.rs
+++ test_5_3/src/main.rs
@@ -4,8 +4,21 @@ fn main() {
         height: 50,
     };
 
+    let rect1 = Rectangle {
+        width: 40,
+        height: 60,
+    };
+
+    let rect2 = Rectangle {
+        width: 50,
+        height: 70,
+    };
+
     println!("the area of rectangle is {} square pixels",rect.area());
     println!("The rectangle has a nonzero width; it is {}",rect.width());
+
+    println!("Can rect1 hold rect2? {}",rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect? {}",rect2.can_hold(&rect));
 }
 
 #[derive(Debug)]
@@ -22,4 +35,10 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
+
+
